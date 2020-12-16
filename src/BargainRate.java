6bcdca4912a2f9112f9067c4558a6454c1fcc30a
Igor <70439007+4esh1r@users.noreply.github.com>
2020-12-16 17:09:03 +0200

    vRODE rABOTAET

--- src/BargainRate.java
+++ src/BargainRate.java
@@ -2,7 +2,7 @@ public class BargainRate extends  Employee{
     private  int tax = 15;
 
     public BargainRate(String name, int salary, String haveChild, String Offshore) {
-        super(name, "bargain rate");
+        super(name, "bargain NALOG");
         if(haveChild.equals("no")) tax+=5;
         if(Offshore.equals("yes")) tax = 0;
         setTax(tax);
--- src/Base.java
+++ src/Base.java
@@ -14,12 +14,12 @@ public class Base {
         int total = 0;
         for (Employee employee : list) {
             System.out.print("Name: " + employee.getName() + ";");
-            System.out.print(" " + "type:" + employee.getType() + ";");
-            System.out.print(" " + "tax:" + employee.getTax() + "%" + ";");
-            System.out.print(" " + "salary:" + employee.getSalary() + ";");
-            System.out.println(" " + "against payment:" + employee.getAgainstPayment() + ";");
+            System.out.print(" " + "tip:" + employee.getType() + ";");
+            System.out.print(" " + "Nalog:" + employee.getTax() + "%" + ";");
+            System.out.print(" " + "saLarY:" + employee.getSalary() + ";");
+            System.out.println(" " + "plati snova:" + employee.getAgainstPayment() + ";");
             total += employee.getSalary();
         }
-        System.out.println("total: " + total);
+        System.out.println("TOTAL: " + total);
     }
 }
